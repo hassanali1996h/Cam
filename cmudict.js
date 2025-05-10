ff0fcdc5deace9afbfbd19123c052be20c729d8c
hassanali1996h <HUGNVF@GMAIL.COM>
2025-05-11 01:47:05 +0300

    Add files via upload

---
+++ cmudict.js
@@ -0,0 +1,14 @@
+const cmuDict = {
+"CIRCLE": "S ER1 K AH0 L",
+"CITY": "S IH1 T IY0",
+"CAT": "K AE1 T",
+"GAME": "G EY1 M",
+"GIRL": "G ER1 L",
+"GOD": "G AA1 D",
+"ZOO": "Z UW1",
+"DESIGN": "D IH0 Z AY1 N",
+"YES": "Y EH1 S",
+"XAVIER": "Z EY1 V Y ER0",
+"TAMEEM": "T AH0 M IY1 M",
+"DOOR": "D AO1 R"
+};
